d4836cb0da4f2730bd9eafc97a13462c232bd82f
Atanas Iliev <68632595+atanas000@users.noreply.github.com>
2023-06-21 23:04:38 +0300

    Create 01.Hello SoftUni.cs

---
+++ 1. First Steps In Coding/01.Hello SoftUni.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace First_Steps_in_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
