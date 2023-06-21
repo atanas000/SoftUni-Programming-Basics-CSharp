0d85b66bd896307c66011e60e463b20b6750606d
Atanas Iliev <68632595+atanas000@users.noreply.github.com>
2023-06-21 23:07:26 +0300

    Create 05. Greeting by Name.cs

---
+++ 1. First Steps In Coding/05. Greeting by Name.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _05.Greeting_by_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
